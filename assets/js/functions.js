757726e683ce2a1ca9e75ab09dfad65dadfa4dd0
Christoph Fricke <christoph@frickeonline.de>
2017-01-01 17:42:14 +0100

    Start Build

---
+++ assets/js/functions.js
@@ -0,0 +1,49 @@
+var token;
+
+function tokenRequest() {
+    var request = new XMLHttpRequest();
+
+    request.open("GET", "assets/php/tokenCreator.php", false);
+    request.send();
+    token = request.responseText;
+}
+
+function processContact() {
+    var name, eMail, message;
+    var statusField = document.getElementById("status");
+    var nameField = document.getElementById("name");
+    var emailField = document.getElementById("email");
+    var messageField = document.getElementById("message");
+    var sendMail = new XMLHttpRequest();
+
+    name = nameField.value;
+    eMail = emailField.value;
+    message = messageField.value;
+
+    if (name == "" || eMail == "" || message == "") {
+        statusField.innerHTML = "Enter in all fields!";
+    } else if (eMail.search("@") == -1) {
+        statusField.innerHTML = "Please use a valid email-adress!";
+    } else {
+        sendMail.open("POST", "assets/php/mail.php", true);
+        sendMail.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        sendMail.send("name=" + name + "&email=" + eMail + "&message=" + message + "&token=" + token);
+    }
+
+    sendMail.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText == 1) {
+                statusField.innerHTML = "Email has been send!";
+            } else {
+                statusField.innerHTML = "Error: Email could not been send!";
+            }
+            nameField.value = "";
+            emailField.value = "";
+            messageField.value = "";
+        }
+    }
+}
+
+function validate(input) {
+
+}
