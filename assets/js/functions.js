cbea42d09dfcd453b6d90611b6a86ebd4cc734d8
Christoph Fricke <christoph@frickeonline.de>
2018-01-09 10:40:07 +0100

    Adds indication for user that he can not send an email...

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,5 @@
 var token;
-var statusField = document.getElementById("status");
+var statusField = document.getElementById('status');
 var sendStatus = false;
 
 /**
@@ -8,14 +8,14 @@ var sendStatus = false;
 function tokenRequest() {
     var request = new XMLHttpRequest();
 
-    request.open("GET", "assets/php/tokenCreator.php", true);
+    request.open('GET', 'assets/php/tokenCreator.php', true);
     request.send();
 
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             token = request.responseText;
         }
-    }
+    };
 }
 
 /**
@@ -23,51 +23,55 @@ function tokenRequest() {
  */
 function processContact() {
     var name, eMail, message, response;
-    var nameField = document.getElementById("name");
-    var emailField = document.getElementById("email");
-    var messageField = document.getElementById("message");
+    var nameField = document.getElementById('name');
+    var emailField = document.getElementById('email');
+    var messageField = document.getElementById('message');
     var sendMail = new XMLHttpRequest();
     name = nameField.value;
     eMail = emailField.value;
     message = messageField.value;
 
-    if (name == "" || eMail == "" || message == "") {
-        statusField.style.color = "#FF9800";
-        statusField.innerHTML = "Warning: All inputs are required.";
-    } else if (eMail.search("@") == -1) {
-        statusField.style.color = "#FF9800";
-        statusField.innerHTML = "Warning: Email-adress is invalid.";
+    if (name == '' || eMail == '' || message == '') {
+        statusField.style.color = '#FF9800';
+        statusField.innerHTML = 'Warning: All inputs are required.';
+    } else if (eMail.search('@') == -1) {
+        statusField.style.color = '#FF9800';
+        statusField.innerHTML = 'Warning: Email-adress is invalid.';
     } else {
-        console.error('Website is outdated and email support has ended!');
-        return;
-        sendMail.open("POST", "assets/php/mail.php", true);
-        sendMail.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        sendMail.send("name=" + name + "&email=" + eMail + "&message=" + message + "&token=" + token);
+        // Indicates that email support has ended.
+        statusField.style.color = '#F44336';
+        statusField.innerHTML = 'Error: Email support has ended for this website.';
+        
+        // sendMail.open('POST', '', true);
+        // sendMail.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        // sendMail.send(
+        //     'name=' + name + '&email=' + eMail + '&message=' + message + '&token=' + token
+        // );
     }
 
     sendMail.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            response = this.responseText.split(";");
+            response = this.responseText.split(';');
             token = response[1];
 
             if (response[0] == 1) {
-                statusField.style.color = "#4CAF50";
-                statusField.innerHTML = "Sucess: Email has been send!";
+                statusField.style.color = '#4CAF50';
+                statusField.innerHTML = 'Sucess: Email has been send!';
 
-                nameField.value = "";
-                emailField.value = "";
-                messageField.value = "";
+                nameField.value = '';
+                emailField.value = '';
+                messageField.value = '';
             } else {
-                statusField.style.color = "#F44336";
-                statusField.innerHTML = "Error: Email could not been send!";
+                statusField.style.color = '#F44336';
+                statusField.innerHTML = 'Error: Email could not been send!';
             }
             sendStatus = true;
         }
-    }
+    };
 }
 
 function resetStatus() {
     if (sendStatus) {
-        statusField.innerHTML = "";
+        statusField.innerHTML = '';
     }
 }
